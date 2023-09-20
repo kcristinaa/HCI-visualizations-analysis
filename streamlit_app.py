13feff6272354613380e572b0655b3dc5c4b8ad1
Christina Karagianni <kara.christi91@gmail.com>
2023-09-20 16:14:45 +0200

    hr+temp-plots

--- streamlit_app.py
+++ streamlit_app.py
@@ -236,6 +236,7 @@ if choose == "Interactive visualizations":
         heartRate_mean = heartRate_mean.rename(columns={'HR': 'Mean'})
         heartRate_mean.reset_index(inplace=True, drop=True)
         heartRate_trend = pd.concat([heartRate_max, heartRate_min, heartRate_mean], axis=1)
+        heartRate_trend['Date'] = pd.to_datetime(heartRate_trend['Date'])
 
         df_male = df[df['Gender'] == 'male']
         df_female = df[df['Gender'] == 'female']
@@ -257,23 +258,168 @@ if choose == "Interactive visualizations":
             '''
         st.markdown(info_hr, unsafe_allow_html=True)
         st.markdown('\n')
-
-        fig = px.line(heartRate_trend, x='Date', y=['Max', 'Min', 'Mean'],
-                      labels={'Date': 'Date', 'value': 'Value'}, color_discrete_sequence=['#ffccb3', '#b0d0e8','#95d0c7' ])
-
+        fig = st.line_chart(heartRate_trend.set_index('Date'), height=300)
         # Customize the layout of the plot
-        fig.update_layout(height = 500, width = 1200, showlegend=True, xaxis_title='Date',
-                          yaxis_title='Heart Rate',xaxis=dict(title_font=dict(size=25),tickfont=dict(size=20)),
-                          yaxis=dict(title_font=dict(size=25),tickfont=dict(size=20)))
-        st.plotly_chart(fig)
+        #fig.update_layout(height = 500, width = 1200, showlegend=True, xaxis_title='Date',
+        ##                  yaxis_title='Heart Rate',xaxis=dict(title_font=dict(size=25),tickfont=dict(size=20)),
+        #                  yaxis=dict(title_font=dict(size=25),tickfont=dict(size=20)))
+        #st.plotly_chart(fig)
+
+        # body temperature plot
 
         st.markdown("""
                            ### Body Temperature Daily Trend 
                            """)
 
+        df['datetime'] = pd.to_datetime(df['datetime'])
+
+        # Split the datetime_column into date and time columns
+        df['date'] = df['datetime'].dt.date
+        df['TEMP'] = pd.to_numeric(df['TEMP'], errors='coerce')
+
+        heartRate = df[['id', 'date', 'TEMP']]
+        heartRate_max = heartRate.groupby('date').agg({'TEMP': 'max'})
+        heartRate_max.reset_index(inplace=True)
+        heartRate_max = heartRate_max.rename(columns={'TEMP': 'Max', 'date': 'Date'})
+        heartRate_min = heartRate.groupby('date').agg({'TEMP': 'min'})
+        heartRate_min = heartRate_min.rename(columns={'TEMP': 'Min'})
+        heartRate_min.reset_index(inplace=True, drop=True)
+        heartRate_mean = round(heartRate.groupby('date').agg({'TEMP': 'mean'}), 1)
+        heartRate_mean = heartRate_mean.rename(columns={'TEMP': 'Mean'})
+        heartRate_mean.reset_index(inplace=True, drop=True)
+        heartRate_trend = pd.concat([heartRate_max, heartRate_min, heartRate_mean], axis=1)
+        heartRate_trend['Date'] = pd.to_datetime(heartRate_trend['Date'])
+
+        df_male = df[df['Gender'] == 'male']
+        df_female = df[df['Gender'] == 'female']
+
+        all_mean = round(df['TEMP'].mean(), 2)
+        male_mean = round(df_male['TEMP'].mean(), 2)
+        female_mean = round(df_female['TEMP'].mean(), 2)
+
+        col1, col2, col3 = st.columns(3)
+        col2.metric(label='Avg. value', value=all_mean)
+        col1.metric(label='Avg. value (Male)', value=male_mean, delta=round(male_mean - all_mean, 2))
+        col3.metric(label='Avg. value (Female)', value=female_mean, delta=round(female_mean - all_mean, 2))
+
+        st.markdown(""" <style> .css-5rimss{font-size: 20px;} </style> """, unsafe_allow_html=True)
+        # info section
+        info_hr = '''
+                   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">                                                                                                    
+                   <i class="fa-solid fa-circle-info" style="color: #0e3f6e;"></i> The plot below compares the min, max and mean body temperature versus the experiment dates for all participants.                                                                                                                          
+                   '''
+        st.markdown(info_hr, unsafe_allow_html=True)
+        st.markdown('\n')
+        fig = st.line_chart(heartRate_trend.set_index('Date'), height=300)
+
+
     if selected == "Self-reports":
 
         st.markdown(""" ## Self-reported Data """)
+
+        df = pd.read_csv('data\data_total.csv', index_col=0)
+        df['DayName'] = pd.to_datetime(df['datetime'])
+        df['DayName'] = df['DayName'].dt.strftime('%a, %Y-%m-%d')
+
+        summer_data = df[['id', 'DayName', 'PA_Score']]
+        winter_data = df[['id', 'DayName', 'NA_Score']]
+
+        sorted_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        summer_data['DayName'] = pd.Categorical(summer_data['DayName'], categories=sorted_days, ordered=True)
+        winter_data['DayName'] = pd.Categorical(winter_data['DayName'], categories=sorted_days, ordered=True)
+
+        df1 = pd.DataFrame(summer_data)
+        df2 = pd.DataFrame(winter_data)
+
+
+        # Function to calculate mean, min, and max values for each day
+        def calculate_aggregates(data, col='PA_Score'):
+            grouped_data = data.groupby('DayName')[col]
+            mean_stress = grouped_data.mean().reset_index()
+            min_stress = grouped_data.min().reset_index()
+            max_stress = grouped_data.max().reset_index()
+            return mean_stress, min_stress, max_stress
+
+
+        df1_mean, df1_min, df1_max = calculate_aggregates(df1, col='PA_Score')
+        df2_mean, df2_min, df2_max = calculate_aggregates(df2, col='NA_Score')
+
+        df1 = df1.merge(df1_mean, on='DayName', suffixes=('', '_mean'))
+        df1 = df1.merge(df1_min, on='DayName', suffixes=('', '_min'))
+        df1 = df1.merge(df1_max, on='DayName', suffixes=('', '_max'))
+        df1['Season of the year'] = 'Summer Term'
+
+        df2 = df2.merge(df2_mean, on='DayName', suffixes=('', '_mean'))
+        df2 = df2.merge(df2_min, on='DayName', suffixes=('', '_min'))
+        df2 = df2.merge(df2_max, on='DayName', suffixes=('', '_max'))
+        df2['Season of the year'] = 'Winter Term'
+
+        combined_df = pd.concat([df1, df2])
+
+        sorted_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        combined_df['DayName'] = pd.Categorical(combined_df['DayName'], categories=sorted_days, ordered=True)
+        sorted_df = combined_df.sort_values(by='DayName')
+
+        df1['term'] = 'Summer Term'
+        df2['term'] = 'Winter Term'
+
+        st.markdown(""" <style> .css-5rimss{font-size: 20px;} </style> """, unsafe_allow_html=True)
+        # info section
+        info_stai = """
+                ### PANAS
+                """
+        st.markdown(info_stai, unsafe_allow_html=True)
+        st.markdown('\n')
+
+        scale = alt.Scale(domain=['Summer Term', 'Winter Term'], range=['#ffccb3', '#95d0c7'])
+
+        line_chart1 = alt.Chart(df1).mark_line(color='#ffccb3').encode(
+            x=alt.X('DayName:N', sort=sorted_days,
+                    axis=alt.Axis(labelFontSize=15, titleFontSize=20, title='Day of the Week')),
+            y=alt.Y('stai_stress_mean:Q',
+                    axis=alt.Axis(labelFontSize=15, title='STAI stress scores', titleFontSize=20)),
+            # color='Season of the year:N',
+            color=alt.Color('Season of the year:N', scale=scale),
+            tooltip=['DayName', 'stai_stress_mean', 'stai_stress_min', 'stai_stress_max']
+        )
+
+        confidence_interval1 = alt.Chart(df1).mark_area(opacity=0.3, color='#ffccb3').encode(
+            x=alt.X('DayName', axis=alt.Axis(labelFontSize=15), sort=sorted_days),
+            y='stai_stress_min',
+            y2='stai_stress_max'
+        )
+
+        line_chart2 = alt.Chart(df2).mark_line(color='#95d0c7').encode(
+            x=alt.X('DayName', axis=alt.Axis(labelFontSize=15), sort=sorted_days),
+            y='stai_stress_mean',
+            # color='Season of the year:N',
+            color=alt.Color('Season of the year:N', scale=scale),
+            tooltip=['DayName', 'stai_stress_mean', 'stai_stress_min', 'stai_stress_max']
+        )
+
+        confidence_interval2 = alt.Chart(df2).mark_area(opacity=0.3, color='#95d0c7').encode(
+            x=alt.X('DayName', axis=alt.Axis(labelFontSize=15), sort=sorted_days),
+            y='stai_stress_min',
+            y2='stai_stress_max'
+        )
+
+        # Combine the line charts and shaded confidence intervals into one interactive chart
+        combined_chart = (line_chart1 + confidence_interval1 + line_chart2 + confidence_interval2).properties(
+            width=800, height=400
+        ).interactive().configure_legend(
+            labelFontSize=20
+        )
+        # Show the interactive chart in Streamlit
+        st.altair_chart(combined_chart, use_container_width=True)
+
+        # finding
+        finding_all = '''
+                        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">                                                                                                    
+                        <i class="fa-solid fa-magnifying-glass" style="color: #2d5986;"></i> As someone could expect, the highest 
+                        stress during the winter months is observed on Sundays before the start of the week. During the summer months, a different pattern is observed probably because the daily routine is altered due to vacations. The highest stress score during the summer months is witnessed on Thursdays.
+                        '''
+        st.markdown(finding_all, unsafe_allow_html=True)
+        st.markdown('\n')
 
 # add footer section
 footer = """
